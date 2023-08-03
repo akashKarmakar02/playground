d92a98d1a258e0a410cb33713315499846ac867f
akashKarmakar02 <akash.anan03@gmail.com>
2023-08-03 18:17:28 +0530

    covered everything with supabase user and modal provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import SideBar from "@/components/SideBar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <SideBar>{children}</SideBar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <SideBar>{children}</SideBar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
