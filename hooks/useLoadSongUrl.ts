624adcea8ac593b346cc2b727200c9e72946345e
akashKarmakar02 <akash.anan03@gmail.com>
2023-08-21 09:58:19 +0530

    hook for getting a songs url

---
+++ hooks/useLoadSongUrl.ts
@@ -0,0 +1,18 @@
+import { Song } from "@/types";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+function useLoadSongUrl(song?: Song) {
+  const supabaseClient = useSupabaseClient();
+
+  if (!song) {
+    return "";
+  }
+
+  const { data } = supabaseClient.storage
+    .from("songs")
+    .getPublicUrl(song.song_path);
+
+  return data.publicUrl;
+}
+
+export default useLoadSongUrl;
