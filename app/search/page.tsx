e5e96da76057b80d36ea63b1f27b4da47edb9c58
akashKarmakar02 <akash.anan03@gmail.com>
2023-08-21 09:51:29 +0530

    added revalidation and searched content to search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,16 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./_components/SearchContent";
 
 interface SearchProps {
   searchParams: {
     title: string;
   };
 }
+
+export const revalidate = 0;
+
 async function page({ searchParams }: SearchProps) {
   const songs = await getSongsByTitle(searchParams.title);
   return (
@@ -17,6 +21,7 @@ async function page({ searchParams }: SearchProps) {
           <SearchInput />
         </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 }
